7d83117f574f9cabe4bd156fa02be4f8dcea77d5
Dan Coleman <coleman2dan@gmail.com>
2025-08-27 21:14:48 -0400

    updated adp

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from draft_engine import DraftGame
 import os
 
 app = Flask(__name__, static_folder="static")
-GAME = DraftGame(players_csv=os.path.join("data", "players5.csv"))
+GAME = DraftGame(players_csv=os.path.join("data", "players6.csv"))
 LAST_SLOT = 4
 
 
